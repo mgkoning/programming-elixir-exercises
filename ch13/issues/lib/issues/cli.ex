b62ae7e4250f3893b8841413758b95441053c3cd
Michiel Koning <github@michiel.ion3.nl>
2022-10-12 22:03:14 +0200

    ch13 ex4 (try again later :()

--- ch13/issues/lib/issues/cli.ex
+++ ch13/issues/lib/issues/cli.ex
@@ -48,6 +48,9 @@ defmodule Issues.CLI do
     |> decode_response()
     |> sort_into_descending_order()
     |> last(count)
+    |> Enum.map(&issue_map_to_tuple/1)
+    |> format_table()
+    |> Enum.each(&IO.puts/1)
   end
 
   defp decode_response({:ok, body}), do: body
@@ -65,5 +68,40 @@ defmodule Issues.CLI do
     list
     |> Enum.take(count)
     |> Enum.reverse
+  end
+
+  def format_table(issues) do
+    headers = {"#", "created_at", "title"}
+    widths = issues
+    |> Enum.map(&triple_to_sizes/1)
+    |> Enum.reduce(triple_to_sizes(headers), &triple_max_by_elem/2)
+    [ headers |> format_line(widths),
+      underline(widths)
+      | issues |> Enum.map(&format_line(&1, widths))]
+  end
+
+  defp triple_to_sizes({a, b, c}), do: { String.length(a), String.length(b), String.length(c) }
+
+  defp triple_max_by_elem({a, b, c}, {d, e, f}), do: { max(a, d), max(b, e), max(c, f) }
+
+  defp underline(widths) do
+    widths
+    |> Tuple.to_list()
+    |> Enum.map(&String.duplicate("-", &1))
+    |> Enum.join("-+-")
+  end
+
+  defp format_line({a, b, c}, {width_a, width_b, width_c}) do
+    "#{center(a, width_a)} | #{String.pad_trailing(b, width_b)} | #{String.pad_trailing(c, width_c)}"
+  end
+
+  defp center(value, width) do
+    value 
+    |> String.pad_leading(width - div(width - String.length(value), 2))
+    |> String.pad_trailing(width)
+  end
+
+  defp issue_map_to_tuple(issue_map) do
+    {"#{issue_map["number"]}", issue_map["created_at"], issue_map["title"]}
   end
 end
